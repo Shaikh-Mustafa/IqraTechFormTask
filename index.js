ebb560a948b8c63d44ec2528fa297aa3ffd8600e
Shaikh Mustafa <66483021+Shaikh-Mustafa@users.noreply.github.com>
2021-12-22 18:16:45 +0530

    Added Image Upload Feature
    Now the user can easily upload and see the image...
    Still lot of bugs

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
- 
-function saveData(){  
 
-    var name = document.querySelector('#name').value;
-    var qualification = document.querySelector('#qualification').value;
-    var place = document.querySelector('#places').value;
-    var experience = document.querySelector('#experience').value;
+    var submit = document.querySelector(".submit");
 
-    // uploaded images....
-    var path = getPath();
+    var uploaded_img = "";
+
+    var upload = document.querySelector(".upload-btn");
 
 
-    // creating a div element.. and adding data-card class
+    upload.addEventListener("click",function(){
+        
+        // creating a div for image
+        var img_div = document.createElement("div");
+        document.body.appendChild(img_div);
+        img_div.classList.add("display_image");
 
-    var div = document.createElement("div");
-    document.body.appendChild(div);
-    div.classList.add("data-card");  
+        // creating an file Reader object and adding an eventListener to it..
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {           
+
+            uploaded_img = reader.result;
+
+            //displaying the image..
+            document.querySelector(".display_image").style.backgroundImage = `url(${uploaded_img})`;
+        })
+
+        reader.readAsDataURL(document.querySelector('.upload_image').files[0]);
+
+        // fake upload change
+        this.innerHTML = "Uploaded";
+        this.classList.add("fake-upload");
+    })
+
+
+    submit.addEventListener("click",function(){
+         // creating a div for storing data...
+         var div = document.createElement("div");
+         document.body.appendChild(div);
+         div.classList.add("data-card");  
+
+        // getting all the four values...
+        var name = document.querySelector('#name').value;
+        var qualification = document.querySelector('#qualification').value;
+        var place = document.querySelector('#places').value;
+        var experience = document.querySelector('#experience').value;
+
+             
+        
+            //getting the last element 
+        var last = (document.querySelectorAll(".data-card").length) - 1;    
     
-     getPath();
+        document.querySelectorAll('.data-card')[last].innerHTML = `Name : ${name} <br> Qualification : ${qualification} <br> Place : ${place} <br> Experience : ${experience} `;
+         
+        //making the image visible...
+        document.querySelector(".display_image").style.display = "inline-block";
+
+        // changing the upload button...
+    })
+
     
-
-    //getting the last element 
-    var last = (document.querySelectorAll(".data-card").length) - 1;    
-   
-    document.querySelectorAll('.data-card')[last].innerHTML = name + "<br>" + qualification + "<br>" + place + "<br>" + experience;
-      
-    
-   
-}
-
-function showImage(){
-    var img = document.createElement("img");
-    var br = document.querySelectorAll(".data-card br");
-
-    document.querySelector(".data-card").insertBefore(img, br);  
-
-    img.classList.add('upload-img');
-
-}
-
-function getPath(){
-    var path = document.querySelector('#image').value;
-    return path;
-}
-
-function fake_upload(){
-    document.querySelector(".upload-btn").innerHTML = "Uploaded";
-    document.querySelector(".upload-btn").classList.add('fake-upload');   
-}
