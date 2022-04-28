339ec9f2fa0e8574eb0fd7949e55b4c1f1a0c928
Shaikh-Mustafa <mrmustafashaikh111@gmail.com>
2022-04-28 23:58:21 +0530

    clean code / remove uncessary css and js/ working properly/ improve User Interface design and update colors

--- index.js
+++ index.js
@@ -1,89 +1,43 @@
 
     var submit = document.querySelector(".submit");
 
-    var uploaded_img = []; 
+    var uploaded_img; 
 
     var upload = document.querySelector(".upload-btn");
 
-   
-
+    function setImage(event){
+        console.log(event.target.files[0]);
+        uploaded_img = event.target.files[0];
+    }
     submit.addEventListener("click",()=>{
         // getting all the users data first....
         var name = document.querySelector('#name').value;
         var qualification = document.querySelector('#qualification').value;
         var place = document.querySelector('#places').value;
         var experience = document.querySelector('#experience').value;
-        var image = document.querySelector(".upload_image").value;
-
-       
-
+        var image_url = URL.createObjectURL(uploaded_img);
+        console.log(image_url)
+         
         // this is where the card is to be created..
         const userSection = document.querySelector("#users_section");
         // creating a card..s_section
         var card = document.createElement("div");
 
-        card.innerHTML = `<h3>Name : <span>${name}</span></h3> 
+        card.innerHTML = `
+        <div class="img-container">
+        <img class='user-img' src = ${image_url} />
+        </div>
+        <div>
+
+        <div>
+        <h3>Name : <span>${name}</span></h3> 
         <h3>Qualification : <span>${qualification}</span></h3>
         <h3>place: <span>${place}</span></h3>
-        <h3>experience: <span>${experience}</span></h3>`
+        <h3>experience: <span>${experience} years</span></h3>
+
+        </div>`
 
         userSection.appendChild(card);
 
         card.classList.add("data-card")
     })
-
-    // upload.addEventListener("click",function(){
-        
-    //     // creating a div for image
-    //     var img_div = document.createElement("div");
-    //     var userSection = document.getElementById("users_section");
-    //     userSection.appendChild(img_div);
-    //     img_div.classList.add("display_image");
-
-    //     // creating an file Reader object and adding an eventListener to it..
-    //     const reader = new FileReader();
-    //     reader.addEventListener("load", () => {           
-
-    //         uploaded_img.push(reader.result);
-
-    //         console.log(uploaded_img);
-    //         console.log(last)
-
-    //         //displaying the image..
-    //         document.querySelectorAll(".display_image")[last].style.backgroundImage = `url(${uploaded_img[last]})`;
-    //     })
-
-    //     reader.readAsDataURL(document.querySelector('.upload_image').files[0]);
-
-    //     // fake upload change
-    //     this.innerHTML = "Uploaded";
-    //     this.classList.add("fake-upload");
-    // })
-
-
-    // submit.addEventListener("click",function(){
-    //      // creating a div for storing data...
-    //      var div = document.createElement("div");
-    //      document.body.appendChild(div);
-    //      div.classList.add("data-card");  
-
-    //     // getting all the four values...
-    //     var name = document.querySelector('#name').value;
-    //     var qualification = document.querySelector('#qualification').value;
-    //     var place = document.querySelector('#places').value;
-    //     var experience = document.querySelector('#experience').value;
-
-             
-        
-    //         //getting the last element 
-    //      last = (document.querySelectorAll(".data-card").length) - 1;    
-    
-    //     document.querySelectorAll('.data-card')[last].innerHTML = `Name : ${name} <br> Qualification : ${qualification} <br> Place : ${place} <br> Experience : ${experience} `;
-         
-    //     //making the image visible...
-    //     document.querySelector(".display_image").style.display = "inline-block";
-
-    //     // changing the upload button...
-    // })
-
-    
